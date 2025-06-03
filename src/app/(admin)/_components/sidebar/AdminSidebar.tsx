ece6be119a0c8248ec9a3e5772db5f7b30f16e02
Phạm Đăng Quang <140483907+QUANGNOTFUN@users.noreply.github.com>
2025-06-03 20:02:05 +0700

    update sidebar

--- src/app/(admin)/_components/sidebar/AdminSidebar.tsx
+++ src/app/(admin)/_components/sidebar/AdminSidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {ComponentType, useEffect, useState} from 'react';
+import React, { ComponentType } from 'react';
 import Link from 'next/link';
 import { LucideProps } from 'lucide-react';
 
@@ -15,48 +15,32 @@ interface AdminSidebarProps {
 }
 
 export function AdminSidebar({ title, items }: AdminSidebarProps) {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [windowWidth, setWindowWidth] = useState<number>(0);
-
-  const handleResize = () => {
-    setWindowWidth(window.innerWidth);
-  }
-
-  useEffect(() => {
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    if (windowWidth >= 1024) {
-      setIsOpen(true);
-    } else {
-      setIsOpen(false);
-    }
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, [windowWidth]);
-
   return (
-    <>
-      {isOpen && (
-        <aside className={`w-64 h-screen bg-gray-50 dark:bg-gray-800 text-black dark:text-white ${title}`}>
-          <div className="p-4 border-b border-gray-700">
-            <h2 className="text-xl font-bold">Admin Panel</h2>
-          </div>
-          <nav className="mt-4">
-            <ul>
-              {items.map((item, index) => (
-                <li key={index} className="mb-2">
-                  <Link href={item.href} className="flex items-center p-3 hover:bg-gray-700 rounded-md transition-colors">
-                    {item.icon && <item.icon className="w-6 h-6 mr-2" /> }
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </aside>
-      )}
-    </>
+    <aside className={`h-screen bg-gray-50/60 dark:bg-zinc-900 text-gray-800 dark:text-gray-100 ${title}`}>
+      <div className="p-4 border-b border-gray-300 dark:border-gray-700">
+        <h2 className="text-center text-xl font-bold">
+          Admin Panel
+        </h2>
+      </div>
+      <nav className="p-4">
+        <ul>
+          {items.map((item, index) => (
+            <li
+              key={index}
+              className="my-4 rounded-lg shadow-xl outline outline-gray-200 dark:outline-gray-600 bg-white dark:bg-zinc-700/10 transform transition-transform duration-200 ease-in-out hover:scale-110"
+            >
+              <Link
+                href={item.href}
+                className="flex items-center p-3 hover:bg-violet-300 dark:hover:bg-slate-500/30 rounded-lg transition-colors"
+              >
+                {item.icon && <item.icon className="w-6 h-6 mr-2 text-gray-600 dark:text-gray-300" />}
+                <span>{item.title}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
   );
 }
 
---
+++ src/app/(admin)/schedule-manage/_values/constants.ts
@@ -0,0 +1,11 @@
+import {AdminSchedulesTableProps} from "@/app/(admin)/_components/AdminSchedulesTable/AdminSchedulesTable";
+
+export const HEADER_SCHEDULE_TABLE: AdminSchedulesTableProps['headers'] = [
+	{ label: "Thứ 2", key: "monday" },
+	{ label: "Thứ 3", key: "tuesday" },
+	{ label: "Thứ 4", key: "wednesday" },
+	{ label: "Thứ 5", key: "thursday" },
+	{ label: "Thứ 6", key: "friday" },
+	{ label: "Thứ 7", key: "saturday" },
+	{ label: "Chủ nhật", key: "sunday" },
+]
