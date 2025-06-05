ffba00f1c0a8f0ca5ec4dc9ca2ebc9e746e7e703
Huy2304 <116505781+Huy2304@users.noreply.github.com>
2025-06-05 15:45:33 +0700

    linh tinh

--- src/app/(guest)/doctor/layout.tsx
+++ src/app/(guest)/doctor/layout.tsx
@@ -12,7 +12,7 @@ export default function DoctorsLayout({ children }: DoctorsLayoutProps) {
     return (
         <ApolloProvider client={apolloClient}>
             <div className="max-w-7xl mx-auto px-4 py-8">
-                <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">Khu vực bác sĩ</h1>
+                <h1 className="text-2xl font-bold text-left text-gray-800 mb-6">Khu vực bác sĩ</h1>
                 {children}
             </div>
         </ApolloProvider>
--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -54,6 +54,39 @@ export default function HomePage() {
                     </a>
                 </div>
             </section>
+            <section className="bg-gray-100 text-black py-16 px-4">
+                <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10">
+
+                    {/* Thông tin liên hệ */}
+                    <div className="bg-white p-8 rounded-2xl shadow-lg">
+                        <h2 className="text-3xl font-bold text-blue-700 mb-6 border-b pb-2">
+                            📬 Thông Tin Liên Hệ
+                        </h2>
+                        <div className="space-y-4 text-gray-700 text-base">
+                            <p>
+                                <span className="font-semibold">📞 Hotline:</span> 1900 123 456
+                            </p>
+                            <p>
+                                <span className="font-semibold">✉️ Email:</span> contact@yte.vn
+                            </p>
+                            <p>
+                                <span className="font-semibold">🏥 Địa chỉ:</span> 123 Đường Sức Khỏe, TP. Hồ Chí Minh
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        className=" p-8 rounded-2xl border-0.5 flex items-center justify-center text-gray-500 italic">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3909.014343986135!2d107.83805711013663!3d11.550828544386283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3173f79dffe37b83%3A0xa46fe528df77f53a!2zTmfDoyAzIFTDoCBOZ8OgbywgTOG7mWMgVGhhbmgsIELhuqNvIEzhu5ljLCBMw6JtIMSQ4buTbmcgMDI2MzMsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1749112486281!5m2!1svi!2s"
+                            width="100%"
+                            height="150%"
+                            style={{ border: 0 }}
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                            title="Địa chỉ Google Map"></iframe>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
